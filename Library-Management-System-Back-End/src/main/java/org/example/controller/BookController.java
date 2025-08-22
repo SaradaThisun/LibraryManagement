e82d11bae44d905b55be8b564a41575c691a1a3d
Sarada Thisun <saradathisun06@gmail.com>
2025-08-22 13:08:56 +0530

    changes done

---
+++ Library-Management-System-Back-End/src/main/java/org/example/controller/BookController.java
@@ -0,0 +1,47 @@
+package org.example.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.dto.Book;
+import org.example.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("book")
+@RequiredArgsConstructor
+public class BookController {
+    @Autowired
+    final BookService service;
+
+    @GetMapping("/get-all")
+    public List<Book> getBook(){
+        return service.getall();
+    }
+
+    @PostMapping("/add-book")
+    public void addBook(@Valid @RequestBody Book book){
+        service.addBook(book);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public Book getBookById(@PathVariable Integer id){
+        return service.searchBookById(id);
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deleteBookById(@PathVariable Integer id){
+        service.deleteBookById(id);
+    }
+
+    @PutMapping("/update-book")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updateBook(@RequestBody Book book){
+        service.updateBookById(book);
+    }
+}
